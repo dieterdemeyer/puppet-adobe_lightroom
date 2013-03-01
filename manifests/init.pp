2bab0645d475a7a726d01de3998f9ee3841a9dbe
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-01 20:47:12 +0100

    Template for installing Adobe Lightroom

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# Public: Install Adobe Lightroom to /Applications
+#
+# Examples
+#
+#  include adobe_lightroom
+class adobe_lightroom {
+  package { '':
+    provider => '',
+    source   => '',
+  }
+}
