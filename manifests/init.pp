79a1d717423d2ac1a008247762ee962aa2edac44
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-02 20:25:30 +0100

    Modified package name

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 #  include adobe_lightroom
 class adobe_lightroom {
-  package { 'Lightroom':
+  package { 'Lightroom_4_LS11_mac_4_3.pkg.dmg':
     provider => 'appdmg',
     source   => 'http://download.adobe.com/pub/adobe/lightroom/mac/4.x/Lightroom_4_LS11_mac_4_3.dmg',
   }
